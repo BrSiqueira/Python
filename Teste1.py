38ca4c8589bcce2a29fc8e0fd78a98eee5782dec
Bruno Siqueira <=>
2017-06-24 16:15:25 -0300

    New

--- Teste1.py
+++ Teste1.py
@@ -1,8 +1,13 @@
-# Python 3: Fibonacci series up to n
-def fib(n):
-    a, b = 0, 1
-    while a < n:
+def fteste(arg):
+    a,b = 0,1
+    while a<arg:
         print(a, end=' ')
-        a, b = b, a+b
+        a,b = b, a+b
     print()
-fib(1000)
+
+fteste(1000)
+print("Testando no Notepadd++")
+print("Não acredito no que estou vendo!!!!")
+print("Aposto que não funciona!")
+print("Nem presisa apertar botão!")
+print(1+1)
