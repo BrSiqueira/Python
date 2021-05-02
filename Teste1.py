0192165a682c1d1a37762972c1c7b657f9f65397
brSiqueira <bruno.siqueira07@gmail.com>
2021-05-02 20:02:47 -0300

    linear regression

--- Teste1.py
+++ Teste1.py
@@ -1,4 +1,4 @@
-# Python 3: Fibonacci series up to n
+el# Python 3: Fibonacci series up to n
 def fib(n):
     a, b = 0, 1
     while a < n:
@@ -7,8 +7,4 @@ def fib(n):
     print()
 fib(1000)
 
-print("Testando no Notepadd++")
-print("Nao acredito no que estou vendo!!!!")
-print("Aposto que nao funciona!")
-print("Nem presisa apertar botao!")
 print(1+1)
